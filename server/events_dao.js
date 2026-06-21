ac9caba9d9d733c6fdb4925a43d7c89ca5413dbf
TommasoOnori <113414042+TommasoOnori@users.noreply.github.com>
2026-06-21 21:46:28 +0200

    feat: Implemented events_dao.js

---
+++ server/events_dao.js
@@ -0,0 +1,13 @@
+import db from './db.js';
+
+export const getEvents = () => {
+    return new Promise((resolve, reject) => {
+        const queryEvents = `SELECT * FROM Events`;
+
+        db.all(queryEvents, [], (err, events) => {
+            if (err) return reject(err);
+            console.log(events);
+            return resolve(events);
+        });
+    });
+};
